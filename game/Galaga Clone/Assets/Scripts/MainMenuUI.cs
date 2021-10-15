5b4e1d82c6ea1cd3b6a90a77c7884665a8ea9ff7
Seth Van Meter <70532030+MincraftEinstein@users.noreply.github.com>
2021-10-15 18:53:16 -0400

    Made the saved game menu a prefab + added blank menus for settings

---
+++ game/Galaga Clone/Assets/Scripts/MainMenuUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public GameObject canvas;
+    public GameObject mainMenu;
+    public GameObject gameMenu;
+
+    public void PlayGameButton()
+    {
+        mainMenu.SetActive(false);
+        Instantiate(gameMenu, canvas.transform);
+    }
+
+    public void SettingsBackButton()
+    {
+        mainMenu.SetActive(true);
+        gameObject.transform.parent.parent.gameObject.SetActive(false);
+    }
+}
