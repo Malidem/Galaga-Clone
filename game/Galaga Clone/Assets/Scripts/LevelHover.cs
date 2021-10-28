dae6fbbe61cc98771d693612a78b40bad95a4cb9
gravitatedReign <83348487+gravitatedReign@users.noreply.github.com>
2021-10-28 13:53:20 -0400

    galaxy menu
    Added the galaxy menu and the first level with hover effect and access to first level

---
+++ game/Galaga Clone/Assets/Scripts/LevelHover.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class LevelHover : MonoBehaviour
+{
+    public GameObject visable;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (IsMouseOver())
+        {
+            visable.SetActive(true);
+        }
+        else
+        {
+            visable.SetActive(false);
+        }
+    }
+
+    private bool IsMouseOver()
+    {
+        PointerEventData pointerEventData = new PointerEventData(EventSystem.current);
+        pointerEventData.position = Input.mousePosition;
+
+        List<RaycastResult> raycastResultsList = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(pointerEventData, raycastResultsList);
+        for (int i = 0; i < raycastResultsList.Count; i++)
+        {
+            if (raycastResultsList[i].gameObject != gameObject && raycastResultsList[i].gameObject != visable)
+            {
+                raycastResultsList.RemoveAt(i);
+                i--;
+            }
+        }
+        return raycastResultsList.Count > 0;
+    }
+}
