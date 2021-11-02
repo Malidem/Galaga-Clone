1d1a8ecf96e50021331e057dc11e46226c08bf18
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2021-11-02 14:17:46 -0400

    Started upgrade cards

---
+++ game/Galaga Clone/Assets/Scripts/UpgradeCard.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeCard : MonoBehaviour
+{
+    private bool isDragging;
+    private bool isOverSlot;
+    private GameObject startParent;
+    private GameObject slot;
+    private Vector2 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDragging)
+        {
+            transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        }
+    }
+
+    public void OnDragStart()
+    {
+        if (isDragging)
+        {
+            return;
+        }
+        startParent = transform.parent.gameObject;
+        startPos = transform.position;
+        isDragging = true;
+    }
+
+    public void OnDragEnd()
+    {
+        isDragging = false;
+
+        if (isOverSlot)
+        {
+            transform.SetParent(slot.transform, false);
+        }
+        else
+        {
+            transform.SetParent(startParent.transform, true);
+            transform.position = startPos;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("CardSlot"))
+        {
+            isOverSlot = true;
+            slot = collision.gameObject;
+        }
+    }
+}
