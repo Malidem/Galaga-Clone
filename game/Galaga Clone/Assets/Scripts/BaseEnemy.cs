5a942457b6a91594de6d518c962de6c2d3ca165b
Seth Van Meter <70532030+MincraftEinstein@users.noreply.github.com>
2022-01-10 18:09:04 -0500

    Ordaga 4 now curves towards the player slightly

--- game/Galaga Clone/Assets/Scripts/BaseEnemy.cs
+++ game/Galaga Clone/Assets/Scripts/BaseEnemy.cs
@@ -21,6 +21,7 @@ public class BaseEnemy : MonoBehaviour
     public int shieldTurretHealth;
 
     private int currentShieldTurretHealth;
+    private bool isOrdaga4;
     private GameManager gameManager;
     private GameObject bulletFolder;
     private GameObject player;
@@ -73,20 +74,42 @@ public class BaseEnemy : MonoBehaviour
             Debug.LogError(gameObject.name + " has a turret, but not defind turret position");
         }
 
-        float backgroundCenter = backgroundRect.rect.height / 2;
-        if (transform.position.y > (backgroundCenter + 175))
+        if (gameObject.name.Contains("Ordaga4"))
         {
-            transform.rotation = Quaternion.Euler(0, 0, 25);
+            isOrdaga4 = true;
+            transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, transform.rotation.y, transform.rotation.z + 180));
         }
-        else if (transform.position.y < (backgroundCenter + -175))
+        else
         {
-            transform.rotation = Quaternion.Euler(0, 0, -25);
+            float backgroundCenter = backgroundRect.rect.height / 2;
+            if (transform.position.y > (backgroundCenter + 175))
+            {
+                transform.rotation = Quaternion.Euler(0, 0, 25);
+            }
+            else if (transform.position.y < (backgroundCenter + -175))
+            {
+                transform.rotation = Quaternion.Euler(0, 0, -25);
+            }
         }
     }
 
     public void Update()
     {
-        transform.Translate(Vector2.left * Time.deltaTime * speed);
+        if (isOrdaga4)
+        {
+            transform.Translate(Vector2.right * Time.deltaTime * speed);
+
+            if (gameManager.gameOver == false)
+            {
+                float angle = Mathf.Atan2(player.transform.position.y - gameObject.transform.position.y, player.transform.position.x - gameObject.transform.position.x) * Mathf.Rad2Deg;
+                Quaternion targetRotation = Quaternion.AngleAxis(angle, Vector3.forward);
+                gameObject.transform.rotation = Quaternion.RotateTowards(gameObject.transform.rotation, targetRotation, 10 * Time.deltaTime); 
+            }
+        }
+        else
+        {
+            transform.Translate(Vector2.left * Time.deltaTime * speed);
+        }
 
         if (transform.position.x > (backgroundRect.rect.width + 25))
         {
